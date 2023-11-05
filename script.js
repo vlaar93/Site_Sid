6b883183572c295aa920ad70fb28e0cefc9742a5
vlaar93 <priamdavid@gmail.com>
2023-11-05 19:02:13 +0100

    1

--- script.js
+++ script.js
@@ -36,14 +36,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const menuIcon = document.querySelector(".menu-icon");
-  const navList = document.querySelector(".nav-list");
-
-  menuIcon.style.display = "block"; // Affichez le menu burger par défaut
-
-  menuIcon.addEventListener("click", function () {
-    navList.classList.toggle("menu-active");
-  });
-});
