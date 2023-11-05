5e58824267a2f880b3e3fa4c8615cc739bfe381a
vlaar93 <priamdavid@gmail.com>
2023-11-05 18:07:36 +0100

    Burger

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".nav-list a");
-    
-    links.forEach(link => {
-        link.addEventListener("click", (e) => {
-            e.preventDefault();
-            const target = document.querySelector(link.getAttribute("href"));
-            if (target) {
-                window.scrollTo({
-                    top: target.offsetTop,
-                    behavior: "smooth"
-                });
-            }
+  const links = document.querySelectorAll(".nav-list a");
+
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = document.querySelector(link.getAttribute("href"));
+      if (target) {
+        window.scrollTo({
+          top: target.offsetTop,
+          behavior: "smooth",
         });
+      }
     });
+  });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const scrollToTopButton = document.getElementById("scroll-to-top");
-    
-    // Affiche le bouton lorsque l'utilisateur fait défiler vers le bas
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 800) {
-            scrollToTopButton.style.display = "block";
-        } else {
-            scrollToTopButton.style.display = "none";
-        }
-    });
+  const scrollToTopButton = document.getElementById("scroll-to-top");
 
-    // Scroll doux vers le haut lorsque le bouton est cliqué
-    scrollToTopButton.addEventListener("click", function () {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    });
+  // Affiche le bouton lorsque l'utilisateur fait défiler vers le bas
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 800) {
+      scrollToTopButton.style.display = "block";
+    } else {
+      scrollToTopButton.style.display = "none";
+    }
+  });
 
+  // Scroll doux vers le haut lorsque le bouton est cliqué
+  scrollToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 });
 
-function toggleMenu() {
-    var menuList = document.querySelector('.nav-list');
-    menuList.classList.toggle('menu-active');
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const navList = document.querySelector(".nav-list");
+
+  menuIcon.style.display = "block"; // Affichez le menu burger par défaut
+
+  menuIcon.addEventListener("click", function () {
+    navList.classList.toggle("menu-active");
+  });
+});
